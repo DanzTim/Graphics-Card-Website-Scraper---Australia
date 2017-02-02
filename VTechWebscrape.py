57e93651ed29c83cb8e7a411d932337f75bcd2d5
Dandi T <kakatake515@gmail.com>
2017-02-02 15:14:26 +0800

    Graphics Card Webscraper for VTechIndustries WA

---
+++ VTechWebscrape.py
@@ -0,0 +1,39 @@
+'''Webscraper for Austin Computers website to get graphics card name and price'''
+
+from bs4 import BeautifulSoup
+import requests
+
+websiteURL = "http://www.vtechindustries.com.au/computer-hardware/video-cards"
+productNum = 0
+pagenum=1
+
+#Function to scrape page based on the given URL above.
+def scrapePage(websiteURL):
+    "Function takes in a URL and scrapes it"
+    r = requests.get(websiteURL)
+    soup = BeautifulSoup(r.content, "lxml")
+
+    productsList = soup.find('ol', class_="products-list")
+    for findProducts in productsList.find_all('h2', class_="product-name"):
+        productName = findProducts.find('a')
+        global productNum;
+        productNum = productNum + 1
+        productPrice = productName.find_next('span', class_="price")
+        #Using str.split() to get rid of everything after "- " for the name. If you want to include it, just remove .split("- ")[0]
+        print repr(productNum).rjust(2), productName.get('title'), productPrice.get_text()
+    return
+
+#Scrapes all the 5 pages for the graphics card section of the Austin Computers website    
+while pagenum <= 2:
+    if pagenum == 1:#Page one does not need extended URL
+        scrapePage(websiteURL)
+    else:           #Page 2 and above needs extended URL using ?p=2.
+        websiteURL = websiteURL+"?p="+str(pagenum)
+        scrapePage(websiteURL)
+    pagenum+=1
+
+
+
+    
+
+
